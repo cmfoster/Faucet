c9e549d3314b0bd2d112b3f0278cb458131f0ae7
cmfoster <curtismfoster@gmail.com>
2012-02-28 10:17:56 -0600

    Renders email templates, will be adding editing for batch mails

--- test/dummy/app/controllers/simulate_controller.rb
+++ test/dummy/app/controllers/simulate_controller.rb
@@ -1,5 +1,5 @@
 class SimulateController < ApplicationController
   def schedule
-    Faucet::Email.create!(:time  => Time.now, :function => "UserMailer.one_week", :description => "One week reminder email", :email => "dshipper@gmail.com")
+    Faucet::Email.create!(:time  => Time.now, :function => "UserMailer.one_week", :description => "One week reminder email", :email => "curtismfoster@gmail.com")
   end
 end
